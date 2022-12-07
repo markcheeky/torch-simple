ab256c11741b93eaa632ebdb825a507432d8357b
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2022-12-07 22:50:24 +0000

    add invalid shape exception

---
+++ torch_simple/exceptions.py
@@ -0,0 +1,7 @@
+from torch import Tensor
+
+
+class IncompatibleShapesError(ValueError):
+    def __init__(self, message: str, tensors: list[Tensor]) -> None:
+        self.tensors = tensors
+        super().__init__(message)
