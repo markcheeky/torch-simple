c37c3a4de0ece4e0aff84bbc44b28b50a25a6f75
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2022-12-07 21:36:41 +0000

    add lambda module

--- torch_simple/nn.py
+++ torch_simple/nn.py
@@ -1,3 +1,5 @@
+from typing import Any, Callable
+
 import torch
 from torch import Tensor
 
@@ -20,3 +22,39 @@ class Constant(torch.nn.Module):
 
     def forward(self, *args, **kwargs) -> Tensor:
         return self.value
+
+
+class Lambda(torch.nn.Module):
+    """
+    Module that wraps an arbitrary function.
+    """
+
+    def __init__(
+        self,
+        fn: Callable,
+        kwargs: dict[str, Any] | None = None,
+        name: str | None = None,
+    ) -> None:
+        super().__init__()
+        self.fn = fn
+        self.name = name
+        if kwargs is None:
+            kwargs = {}
+        self.addictional_kwargs = kwargs
+
+    def forward(self, *args: Any, **kwargs: Any) -> Any:
+        return self.fn(*args, **kwargs, **self.addictional_kwargs)
+
+    def extra_repr(self) -> str:
+        if self.name is not None:
+            name = self.name
+        else:
+            name = self.fn.__name__
+
+        if len(self.addictional_kwargs) > 0:
+            additional = ", ".join(
+                f"{k}={v}" for k, v in self.addictional_kwargs.items()
+            )
+            return f"fn={name}(..., {additional})"
+        else:
+            return f"fn={name}"
