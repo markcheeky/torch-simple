64a68b7e2cd2c3f51694518cc67072e57cd90630
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2022-12-08 18:44:22 +0000

    add basic tests for residual module

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -1,13 +1,17 @@
 import itertools
+import typing
 
+import pytest
 import torch
 from torch import Tensor
 
-from torch_simple.nn import Constant
+from torch_simple.exceptions import IncompatibleShapesError
+from torch_simple.functional import Side, truncate_to_shape
+from torch_simple.nn import Constant, Lambda, Residual
 
 
 class TestConstant:
-    def test_layer_outputs_correctly(self):
+    def test_layer_outputs_correctly(self) -> None:
         values = [
             torch.zeros(0),
             torch.zeros(1),
@@ -34,7 +38,7 @@ class TestConstant:
                 layer = Constant(value, trainable=trainable)
                 assert torch.allclose(layer(*args, **kwargs), value)
 
-    def test_layer_is_trainable(self):
+    def test_layer_is_trainable(self) -> None:
         orig_value = torch.ones(1, 2, 3)
         layer = Constant(orig_value, trainable=True)
         assert sum(param.requires_grad for param in layer.parameters()) == 1
@@ -46,6 +50,197 @@ class TestConstant:
         optimizer.step()
         assert not torch.allclose(layer.value, orig_value)
 
-    def test_layer_is_not_trainable(self):
+    def test_layer_is_not_trainable(self) -> None:
         layer = Constant(torch.ones(1, 2, 3), trainable=False)
         assert sum(param.requires_grad for param in layer.parameters()) == 0
+
+
+class TestResidual:
+    def test_layer_outputs_correctly(self) -> None:
+
+        model_lin_input = (2, 10)
+        model_lin = torch.nn.Linear(10, 10)
+
+        model_ff_input = (4, 10)
+        model_ff = torch.nn.Sequential(
+            torch.nn.Linear(10, 10),
+            torch.nn.Tanh(),
+            torch.nn.Linear(10, 10),
+            torch.nn.Tanh(),
+        )
+
+        model_conv_input = (4, 5, 10, 10)
+        model_conv = torch.nn.Sequential(
+            torch.nn.Conv2d(5, 5, (3, 3), padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(5, 5, (3, 3), padding=1),
+            torch.nn.ReLU(),
+        )
+
+        model_gru_input = (15, 4, 8)
+        model_gru = torch.nn.Sequential(
+            torch.nn.GRU(
+                input_size=8,
+                hidden_size=8,
+                num_layers=4,
+                batch_first=False,
+            ),
+            Lambda(lambda out_and_h: out_and_h[0]),
+        )
+
+        model_transformer_input = (15, 4, 64)
+        model_transformer = torch.nn.TransformerEncoder(
+            torch.nn.TransformerEncoderLayer(
+                d_model=64,
+                nhead=4,
+                dim_feedforward=128,
+            ),
+            num_layers=3,
+        )
+
+        for model_input, model in [
+            (model_lin_input, model_lin),
+            (model_ff_input, model_ff),
+            (model_conv_input, model_conv),
+            (model_gru_input, model_gru),
+            (model_transformer_input, model_transformer),
+        ]:
+            x = torch.randn(model_input)
+            res_model = Residual(model).eval()
+            assert torch.allclose(res_model(x), x + model(x))
+
+            res_model = Residual(model, enabled=False).eval()
+            assert torch.allclose(res_model(x), model(x))
+
+    def test_layer_with_padding(self) -> None:
+        model_lin_input = (2, 10)
+        model_lin = torch.nn.Linear(10, 20)
+
+        model_ff_input = (4, 10)
+        model_ff = torch.nn.Sequential(
+            torch.nn.Linear(10, 20),
+            torch.nn.Tanh(),
+            torch.nn.Linear(20, 20),
+            torch.nn.Tanh(),
+        )
+
+        model_conv_input = (4, 5, 10, 10)
+        model_conv = torch.nn.Sequential(
+            torch.nn.Conv2d(5, 5, (3, 3), padding=4),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(5, 5, (3, 3), padding=4),
+            torch.nn.ReLU(),
+        )
+
+        model_gru_input = (15, 4, 8)
+        model_gru = torch.nn.Sequential(
+            torch.nn.GRU(
+                input_size=8,
+                hidden_size=20,
+                num_layers=4,
+                batch_first=False,
+            ),
+            Lambda(lambda out_and_h: out_and_h[0]),
+        )
+
+        model_transformer_input = (15, 4, 32)
+        model_transformer = torch.nn.Sequential(
+            torch.nn.Linear(32, 64),
+            torch.nn.TransformerEncoder(
+                torch.nn.TransformerEncoderLayer(
+                    d_model=64,
+                    nhead=4,
+                    dim_feedforward=128,
+                ),
+                num_layers=3,
+            ),
+        )
+
+        for model_input, model in [
+            (model_lin_input, model_lin),
+            (model_ff_input, model_ff),
+            (model_conv_input, model_conv),
+            (model_gru_input, model_gru),
+            (model_transformer_input, model_transformer),
+        ]:
+            assert isinstance(model, torch.nn.Module)
+            model = model.eval()
+            x = torch.randn(model_input)
+
+            with pytest.raises(IncompatibleShapesError):
+                res_model = Residual(model).eval()
+                res_model(x)
+
+            for side in typing.get_args(Side):
+                res_model = Residual(model, pad_if_needed=True, align_on_pad=side).eval()
+                assert torch.allclose(
+                    truncate_to_shape(res_model(x), x.shape, keep_side=side),
+                    x + truncate_to_shape(model(x), x.shape, keep_side=side),
+                )
+
+    def test_layer_with_truncation(self) -> None:
+
+        model_lin_input = (2, 10)
+        model_lin = torch.nn.Linear(10, 5)
+
+        model_ff_input = (4, 10)
+        model_ff = torch.nn.Sequential(
+            torch.nn.Linear(10, 20),
+            torch.nn.Tanh(),
+            torch.nn.Linear(20, 5),
+            torch.nn.Tanh(),
+        )
+
+        model_conv_input = (4, 5, 10, 10)
+        model_conv = torch.nn.Sequential(
+            torch.nn.Conv2d(5, 5, (3, 3)),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(5, 5, (3, 3)),
+            torch.nn.ReLU(),
+        )
+
+        model_gru_input = (15, 4, 20)
+        model_gru = torch.nn.Sequential(
+            torch.nn.GRU(
+                input_size=20,
+                hidden_size=8,
+                num_layers=4,
+                batch_first=False,
+            ),
+            Lambda(lambda out_and_h: out_and_h[0]),
+        )
+
+        model_transformer_input = (15, 4, 64)
+        model_transformer = torch.nn.Sequential(
+            torch.nn.Linear(64, 32),
+            torch.nn.TransformerEncoder(
+                torch.nn.TransformerEncoderLayer(
+                    d_model=32,
+                    nhead=4,
+                    dim_feedforward=128,
+                ),
+                num_layers=3,
+            ),
+        )
+
+        for model_input, model in [
+            (model_lin_input, model_lin),
+            (model_ff_input, model_ff),
+            (model_conv_input, model_conv),
+            (model_gru_input, model_gru),
+            (model_transformer_input, model_transformer),
+        ]:
+            assert isinstance(model, torch.nn.Module)
+            model = model.eval()
+            x = torch.randn(model_input)
+
+            with pytest.raises(IncompatibleShapesError):
+                res_model = Residual(model).eval()
+                res_model(x)
+
+            for side in typing.get_args(Side):
+                res_model = Residual(model, truncate_if_needed=True, keep_on_truncate=side).eval()
+                res_pred = res_model(x)
+                expected = model(x) + truncate_to_shape(x, res_pred.shape, keep_side=side)
+
+                assert torch.allclose(res_pred, expected)
--- torch_simple/nn.py
+++ torch_simple/nn.py
@@ -1,7 +1,10 @@
-from typing import Any, Callable
+from typing import Any, Callable, Iterable
 
 import torch
 from torch import Tensor
+
+from torch_simple.exceptions import IncompatibleShapesError
+from torch_simple.functional import Side, pad_to_shape, truncate_to_shape
 
 
 class Constant(torch.nn.Module):
@@ -52,9 +55,160 @@ class Lambda(torch.nn.Module):
             name = self.fn.__name__
 
         if len(self.addictional_kwargs) > 0:
-            additional = ", ".join(
-                f"{k}={v}" for k, v in self.addictional_kwargs.items()
-            )
+            additional = ", ".join(f"{k}={v}" for k, v in self.addictional_kwargs.items())
             return f"fn={name}(..., {additional})"
         else:
             return f"fn={name}"
+
+
+class Residual(torch.nn.Module):
+    """
+    Simple residual wrapper that adds a residual connection around a module.
+
+    If the input and residual (output) shapes are the same, the input is added to the output.
+    It can also pad/truncate the one to match the output shape of the other.
+
+
+    >>> import torch
+    >>> x = torch.randn(2, 10)
+    >>> block = torch.nn.Linear(10, 10)
+    >>> model = Residual(block)
+    >>> torch.allclose(model(x), x + block(x))
+    True
+
+    >>> x = torch.randn(4, 5, 10, 10)
+    >>> block = torch.nn.Conv2d(5, 5, (3, 3), padding=1)
+    >>> model = Residual(block)
+    >>> torch.allclose(model(x), x + block(x))
+    True
+
+    """
+
+    def __init__(
+        self,
+        around: torch.nn.Module,
+        enabled: bool = True,
+        learn_skipping_weight: bool = False,
+        learn_residual_weight: bool = False,
+        pad_if_needed: bool = False,
+        truncate_if_needed: bool = False,
+        align_on_pad: Side | Iterable[Side] | None = "middle",
+        keep_on_truncate: Side | Iterable[Side] | None = "middle",
+        match_input_shape: bool = False,
+    ) -> None:
+        """
+        Args:
+            around:
+                The module to skip around with residual connection
+
+            enabled:
+                Whether to enable the residual connection
+                if False, it will just return the output of the inner module
+
+            learn_skipping_weight:
+                Whether to learn a weight for the skipping connection.
+                By default, the weight is 1.0
+
+            learn_residual_weight:
+                Whether to learn a weight for the residual connection
+                By default, the weight is 1.0
+
+            pad_if_needed:
+                Whether to pad the input to match the output shape
+                if ouput is larger than input
+
+            truncate_if_needed:
+                Whether to truncate the input to match the output shape
+                if output is smaller than input
+
+            align_on_pad:
+                If pad_if_needed is True, how to align the input on the output shape.
+
+            keep_on_truncate:
+                If truncate_if_needed is True, which part of the input to keep when truncating.
+
+            match_input_shape:
+                If False, output shape will be the same as residual shape
+                    -> input shape will be padded/truncated to match.
+                If True, output shape will be the same as input shape
+                    -> residual shape will be padded/truncated to match.
+        """
+
+        super().__init__()
+
+        if align_on_pad is None:
+            align_on_pad = "middle"
+
+        if keep_on_truncate is None:
+            keep_on_truncate = "middle"
+
+        self.around = around
+        self.enabled = enabled
+        self.pad_if_needed = pad_if_needed
+        self.truncate_if_needed = truncate_if_needed
+        self.align_on_pad = align_on_pad
+        self.keep_on_truncate = keep_on_truncate
+        self.match_input_shape = match_input_shape
+
+        self.skipping_weight: torch.nn.Parameter | None = None
+        self.residual_weight: torch.nn.Parameter | None = None
+
+        if learn_skipping_weight:
+            self.skipping_weight = torch.nn.Parameter(torch.tensor(1.0))
+
+        if learn_residual_weight:
+            self.residual_weight = torch.nn.Parameter(torch.tensor(1.0))
+
+    def forward(self, x: Tensor) -> Tensor:
+        y: Tensor = self.around(x)
+        if not self.enabled:
+            return y
+
+        if len(x.shape) != len(y.shape):
+            raise IncompatibleShapesError(
+                "Input and output shapes must have same number of dimensions, "
+                f"got {x.shape} and {y.shape}",
+                [x, y],
+            )
+
+        if not self.truncate_if_needed and any(
+            dim_x > dim_y for dim_x, dim_y in zip(x.shape, y.shape)
+        ):
+            raise IncompatibleShapesError(
+                f"Input shape {x.shape} is larger "
+                f"than output shape {y.shape} and truncate_if_needed=False",
+                [x, y],
+            )
+
+        if not self.pad_if_needed and any(dim_x < dim_y for dim_x, dim_y in zip(x.shape, y.shape)):
+            raise IncompatibleShapesError(
+                f"Input shape {x.shape} is smaller "
+                f"than output shape {y.shape} and pad_if_needed=False",
+                [x, y],
+            )
+
+        if self.truncate_if_needed:
+            if self.match_input_shape:
+                y = truncate_to_shape(y, x.shape, self.keep_on_truncate)
+            else:
+                x = truncate_to_shape(x, y.shape, self.keep_on_truncate)
+
+        if self.pad_if_needed:
+            if self.match_input_shape:
+                y = pad_to_shape(y, x.shape, self.align_on_pad)
+            else:
+                x = pad_to_shape(x, y.shape, self.align_on_pad)
+
+        if self.skipping_weight is not None:
+            x = x * self.skipping_weight
+
+        if self.residual_weight is not None:
+            y = y * self.residual_weight
+
+        return x + y
+
+    def extra_repr(self) -> str:
+        extra = []
+        extra.append(f"learn_skipping_weight={self.skipping_weight is not None}")
+        extra.append(f"learn_residual_weight={self.residual_weight is not None}")
+        return ", ".join(extra)
