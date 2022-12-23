0dbd4141af2c599798cc6e84a0bb169c697d85bd
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2022-12-23 11:27:59 +0000

    add type aliases for activation, dropout and norm layer

--- torch_simple/typedefs.py
+++ torch_simple/typedefs.py
@@ -1,3 +1,20 @@
-from typing import Literal
+from __future__ import annotations
+
+from typing import Callable, Literal, Type, TypeAlias
+
+import torch
+from torch import Tensor
 
 Side = Literal["start", "end", "middle"]
+
+ActivationLike: TypeAlias = (
+    str | torch.nn.Module | Callable[[Tensor], Tensor] | Type | Literal["none"] | None
+)
+
+DropoutLike: TypeAlias = (
+    float | torch.nn.Module | Callable[[Tensor], Tensor] | Type | Literal["none"] | None
+)
+
+NormLayerLike: TypeAlias = (
+    str | torch.nn.Module | Callable[[Tensor], Tensor] | Type | Literal["none"] | None
+)
